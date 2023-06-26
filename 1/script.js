83fced72039fb79a285dafaef0f364215b157eeb
leo-fed <fed.leo.id@gmail.com>
2023-06-26 14:03:27 +0300

    Optimize code

--- 1/script.js
+++ 1/script.js
@@ -3,11 +3,7 @@ const icons = [...document.querySelectorAll(".arrow")];
 
 btn.addEventListener("click", replacement)
 function replacement() {
-    icons.forEach( (e) => {
-        if (e.classList.contains("arrow_hidden")) {
-            e.classList.remove("arrow_hidden")
-        } else {
-            e.classList.add("arrow_hidden")
-        }
+    icons.forEach( (icon) => {
+        icon.classList.toggle("arrow_hidden")
     })
 }
